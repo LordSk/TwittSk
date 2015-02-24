9a858b81024f7d7fed5d74e4c2303e9c08a224bc
LordSk <lordskelethom@gmail.com>
2015-02-24 18:29:52 +0100

    Doc

--- netrequestfactory.h
+++ netrequestfactory.h
@@ -14,19 +14,47 @@ class NetRequestFactory
     const QString _consumerKeySecret;
     const QString _tokenSecret;
 
+    // Simple random class
     struct Random {
         std::mt19937 gen;
         int getInt(int min, int max);
     } _rand;
 
+    /**
+     * @brief Get unix timestamp
+     * @return unix timestamp
+     */
     int64_t getTimestamp() const;
+
+    /**
+     * @brief Generate a 32 char long random string
+     * @return random string
+     */
     QString genUniqueToken();
+
+    /**
+     * @brief Generate an authorization header
+     * @param url base url
+     * @return authorization header
+     */
     QString genAuthHeader(const QUrl& url);
+
+    /**
+     * @brief Generate an OAuth signature
+     * @param url base url
+     * @param nonce unique token
+     * @param timestamp unix timestamp
+     * @return OAuth signature
+     */
     QString genSignature(const QUrl& url, const QString &nonce, const QString &timestamp);
 
 public:
     NetRequestFactory();
 
+    /**
+     * @brief Create a home timeline request
+     * @return home timeline request
+     */
     QNetworkRequest homeTimeline();
 };
 
